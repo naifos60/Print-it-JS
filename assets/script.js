cb3b0612cd4831a9fca4053ee1ad0f0dd894ebb0
naifos60 <naifos60@hotmail.com>
2023-04-18 06:29:49 +0200

    feature changement image et changement texte au clic

--- assets/script.js
+++ assets/script.js
@@ -16,11 +16,22 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let i = 0;
+
 const arrowRight = document.querySelector(".arrow_right");
 arrowRight.addEventListener("click", function(){
 	console.log("suivant");
-	document.querySelector(".dot_selected").nextElementSibling.classList.add("dot_selected");
-	document.querySelector(".dot_selected").classList.remove("dot_selected");
+		document.querySelector(".dot_selected").nextElementSibling.classList.add("dot_selected");
+	    document.querySelector(".dot_selected").classList.remove("dot_selected");
+	
+	if(i > 3){	
+		i = 0;
+	}else{
+		i++;
+	}
+	console.log(i);
+	document.querySelector(".banner-img").setAttribute("src", "./assets/images/slideshow/" + slides[i].image);
+	document.querySelector(".banner-img").nextElementSibling.innerHTML = slides[i].tagLine;
 });
 
 const arrowLeft = document.querySelector(".arrow_left");
@@ -28,6 +39,13 @@ arrowLeft.addEventListener("click", function(){
 	console.log("précédent");
 	document.querySelector(".dot_selected").previousElementSibling.classList.add("dot_selected");
 	document.querySelector(".dot_selected").nextElementSibling.classList.remove("dot_selected");
+	if(i < 0){	
+		i = 3;
+	}else{
+		i--;
+	}
+	document.querySelector(".banner-img").setAttribute("src", "./assets/images/slideshow/" + slides[i].image);
+	document.querySelector(".banner-img").nextElementSibling.innerHTML = slides[i].tagLine;
 });
 
 
